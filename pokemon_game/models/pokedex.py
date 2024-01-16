a75cea477905f567a0728445f19abefb66104ed7
hugo-esquer <hugo.esquer@laplateforme.io>
2024-01-16 12:14:38 +0100

    création pokedex.json et pokedex.py

---
+++ pokemon_game/models/pokedex.py
@@ -0,0 +1,16 @@
+import pygame
+from menupokemon import menuPokemon
+
+pygame.init()
+
+largeur_fenetre = 800
+hauteur_fenetre = 480
+fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre))
+pygame.display.set_caption("Pokédex")
+
+NOIR = (0,0,0)
+
+fond = pygame.image.load("pokemon_game\\images\\BG-dex.png")
+
+menu_pokemon = menuPokemon(fond, "pokemon_game\\pokedex.json", largeur_fenetre, hauteur_fenetre, fenetre)
+menu_pokemon.pokedex()
