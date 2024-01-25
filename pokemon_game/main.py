94a2c520f9aa5ee028a89195e7c4dd8cd7f88236
erika-aschehoug <erika.aschehoug@laplateforme.io>
2024-01-25 11:27:25 +0100

    changement menu

--- pokemon_game/main.py
+++ pokemon_game/main.py
@@ -1,192 +1,85 @@
 import pygame
- 
-surfaceW = 800 #Dimension de la fenêtre / Largeur
-surfaceH = 480 #Dimension de la fenêtre / Longueur
- 
-class Menu :
-    """ Création et gestion des boutons d'un menu """
-    def __init__(self, application, *groupes) :
-        self.couleurs = dict(
-            normal=(255, 165, 0),
-            survol=(173, 21, 230),
-        )
-        font = pygame.font.SysFont('Bombardier', 24)
-        # noms des menus et commandes associées
-        items = (
-            ('JOUER', application.jeu),
-            ('QUITTER', application.quitter)
-        )
-        x = 400
-        y = 200
-        self._boutons = []
-        for texte, cmd in items :
-            mb = MenuBouton(
-                texte,
-                self.couleurs['normal'],
-                font,
-                x,
-                y,
-                200,
-                50,
-                cmd
-            )
-            self._boutons.append(mb)
-            y += 120
-            for groupe in groupes :
-                groupe.add(mb)
- 
-    def update(self, events) :
-        clicGauche, *_ = pygame.mouse.get_pressed()
-        posPointeur = pygame.mouse.get_pos()
-        for bouton in self._boutons :
-            # Si le pointeur souris est au-dessus d'un bouton
-            if bouton.rect.collidepoint(*posPointeur) :
-                # Changement du curseur par un quelconque
-                pygame.mouse.set_cursor(*pygame.cursors.tri_left)
-                # Changement de la couleur du bouton
-                bouton.dessiner(self.couleurs['survol'])
-                # Si le clic gauche a été pressé
-                if clicGauche :
-                    # Appel de la fonction du bouton
-                    bouton.executerCommande()
-                break
-            else :
-                # Le pointeur n'est pas au-dessus du bouton
-                bouton.dessiner(self.couleurs['normal'])
-        else :
-            # Le pointeur n'est pas au-dessus d'un des boutons
-            # initialisation au pointeur par défaut
-            pygame.mouse.set_cursor(*pygame.cursors.arrow)
- 
-    def detruire(self) :
-        pygame.mouse.set_cursor(*pygame.cursors.arrow) # initialisation du pointeur
- 
- 
- 
-class MenuBouton(pygame.sprite.Sprite) :
-    """ Création d'un simple bouton rectangulaire """
-    def __init__(self, texte, couleur, font, x, y, largeur, hauteur, commande) :
-        super().__init__()
-        self._commande = commande
- 
-        self.image = pygame.Surface((largeur, hauteur))
- 
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
- 
-        self.texte = font.render(texte, True, (0, 0, 0))
-        self.rectTexte = self.texte.get_rect()
-        self.rectTexte.center = (largeur/2, hauteur/2)
- 
-        self.dessiner(couleur)
- 
-    def dessiner(self, couleur) :
-        self.image.fill(couleur)
-        self.image.blit(self.texte, self.rectTexte)
- 
-    def executerCommande(self) :
-        # Appel de la commande du bouton
-        self._commande()
- 
- 
-class Jeu :
-    """ Simulacre de l'interface du jeu """
-    def __init__(self, jeu, *groupes) :
-        self._fenetre = jeu.fenetre
-        jeu.fond = (0, 0, 0)
- 
-        from itertools import cycle
-        couleurs = [(0, 48, i) for i in range(0, 256, 15)]
-        couleurs.extend(sorted(couleurs[1:-1], reverse=True))
-        self._couleurTexte = cycle(couleurs)
- 
-        self._font = pygame.font.SysFont('Helvetica', 36, bold=True)
-        self.creerTexte()
-        self.rectTexte = self.texte.get_rect()
-        self.rectTexte.center = (surfaceW/2, surfaceH/2)
-        # Création d'un event
-        self._CLIGNOTER = pygame.USEREVENT + 1
-        pygame.time.set_timer(self._CLIGNOTER, 80)
- 
-    def creerTexte(self) :
-        self.texte = self._font.render(
-            'LE JEU EST EN COURS D\'EXÉCUTION',
-            True,
-            next(self._couleurTexte)
-        )
- 
-    def update(self, events) :
-        self._fenetre.blit(self.texte, self.rectTexte)
-        for event in events :
-            if event.type == self._CLIGNOTER :
-                self.creerTexte()
-                break
- 
-    def detruire(self) :
-        pygame.time.set_timer(self._CLIGNOTER, 0) # désactivation du timer
- 
- 
-class Application :
-    """ Classe maîtresse gérant les différentes interfaces du jeu """
-    def __init__(self) :
-        pygame.init()
-        pygame.display.set_caption("Pokemon")
-        image_fond = pygame.image.load("pokemon_game\\images\\background.accueil.jpg")
-        image_fond = pygame.transform.scale(image_fond, (surfaceW, surfaceH))
-        self.fond = image_fond
- 
+import sys
+import subprocess
 
- 
-        self.fenetre = pygame.display.set_mode((surfaceW,surfaceH))
-        # Groupe de sprites utilisé pour l'affichage
-        self.groupeGlobal = pygame.sprite.Group()
-        self.statut = True
- 
-    def _initialiser(self) :
-        try:
-            self.ecran.detruire()
-            # Suppression de tous les sprites du groupe
-            self.groupeGlobal.empty()
-        except AttributeError:
-            pass
- 
-    def menu(self) :
-        # Affichage du menu
-        self._initialiser()
-        self.ecran = Menu(self, self.groupeGlobal)
- 
-    def jeu(self) :
-        # # Affichage du jeu
-        # self._initialiser()
-        # self.ecran = Jeu(self, self.groupeGlobal)
-        import menu
-        menu.py
- 
-    def quitter(self) :
-        self.statut = False
- 
-    def update(self) :
-        events = pygame.event.get()
- 
-        for event in events :
-            if event.type == pygame.QUIT :
-                self.quitter()
-                return
- 
-        self.fenetre.blit(self.fond, (0,0))
-        self.ecran.update(events)
-        self.groupeGlobal.update()
-        self.groupeGlobal.draw(self.fenetre)
-        pygame.display.update()
- 
- 
-app = Application()
-app.menu()
- 
-clock = pygame.time.Clock()
- 
-while app.statut :
-    app.update()
-    clock.tick(30)
- 
-pygame.quit()
+# Initialisation de Pygame
+pygame.init()
+
+# Paramètres de la fenêtre
+largeur_fenetre = 800
+hauteur_fenetre = 480
+
+# Image de fond
+image_fond = pygame.image.load("pokemon_game\\images\\background.accueil.jpg")
+image_fond = pygame.transform.scale(image_fond, (largeur_fenetre, hauteur_fenetre))
+
+# Couleurs
+couleur_bouton_normal = (255, 165, 0)
+couleur_bouton_survol = (255, 200, 0)
+couleur_bouton_clic = (255, 100, 0)
+couleur_texte = (0, 0, 0)
+
+# Création de la fenêtre
+fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre))
+pygame.display.set_caption("Pokemon")
+
+# Fonction pour dessiner un bouton
+def dessiner_bouton(x, y, largeur, hauteur, texte, couleur):
+    pygame.draw.rect(fenetre, couleur, (x, y, largeur, hauteur))
+    
+    font = pygame.font.SysFont('Bombardier', 24)
+    texte_surface = font.render(texte, True, couleur_texte)
+    texte_rect = texte_surface.get_rect(center=(x + largeur // 2, y + hauteur // 2))
+    
+    fenetre.blit(texte_surface, texte_rect)
+
+# Obtenir les dimensions du bouton
+largeur_bouton = 300
+hauteur_bouton = 50
+
+# Calculer les positions des boutons pour les centrer
+x_bouton = (largeur_fenetre - largeur_bouton) // 2
+y_bouton1 = (hauteur_fenetre - (hauteur_bouton * 2 + 10)) // 2
+y_bouton2 = y_bouton1 + hauteur_bouton + 60
+# Définir la couleur initiale des boutons
+couleur_bouton1 = couleur_bouton_normal
+couleur_bouton2 = couleur_bouton_normal
+
+# Boucle principale
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # Gérer les événements de la souris pour changer la couleur des boutons
+        if event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
+            if x_bouton < x < x_bouton + largeur_bouton and y_bouton1 < y < y_bouton1 + hauteur_bouton:
+                couleur_bouton1 = couleur_bouton_survol
+            else:
+                couleur_bouton1 = couleur_bouton_normal
+
+            if x_bouton < x < x_bouton + largeur_bouton and y_bouton2 < y < y_bouton2 + hauteur_bouton:
+                couleur_bouton2 = couleur_bouton_survol
+            else:
+                couleur_bouton2 = couleur_bouton_normal
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if x_bouton < x < x_bouton + largeur_bouton and y_bouton1 < y < y_bouton1 + hauteur_bouton:
+                couleur_bouton1 = couleur_bouton_clic
+
+            if x_bouton < x < x_bouton + largeur_bouton and y_bouton2 < y < y_bouton2 + hauteur_bouton:
+                couleur_bouton2 = couleur_bouton_clic
+
+
+    fenetre.blit(image_fond, (0, 0))
+
+    # Dessiner les boutons
+    dessiner_bouton(x_bouton, y_bouton1, largeur_bouton, hauteur_bouton, "ACCUEIL", couleur_bouton1)
+    dessiner_bouton(x_bouton, y_bouton2, largeur_bouton, hauteur_bouton, "QUITTER", couleur_bouton2)
+    
+
+    pygame.display.flip()
