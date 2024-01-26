2a7b49127650be0b7a8bafd68617ed69d04cf72a
erika-aschehoug <erika.aschehoug@laplateforme.io>
2024-01-26 10:28:58 +0100

    ajout return

--- pokemon_game/main.py
+++ pokemon_game/main.py
@@ -57,9 +57,12 @@ class PokemonGame:
                     x, y = event.pos
                     if self.x_bouton < x < self.x_bouton + self.largeur_bouton and self.y_bouton1 < y < self.y_bouton1 + self.hauteur_bouton:
                         self.couleur_bouton1 = self.couleur_bouton_clic
+                        return "menu"
 
                     if self.x_bouton < x < self.x_bouton + self.largeur_bouton and self.y_bouton2 < y < self.y_bouton2 + self.hauteur_bouton:
                         self.couleur_bouton2 = self.couleur_bouton_clic
+                        pygame.quit()
+                        sys.exit()
 
             self.fenetre.blit(self.image_fond, (0, 0))
 
